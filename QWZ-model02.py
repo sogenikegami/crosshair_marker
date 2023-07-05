594472b652ec2050f942d884d2402c0c81a21dac
sogenikegami <ikegami-sogen443@g.ecc.u-tokyo.ac.jp>
2023-07-05 14:24:22 +0900

    update QWZ

--- QWZ-model02.py
+++ QWZ-model02.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+import matplotlib.patches as pat
 import matplotlib.colors as mcolors
 import seaborn as sns
 import crosshair_marker01
@@ -9,12 +10,12 @@ sys.path.append('..')
 from local_chern_marker import local_chern_marker01
 
 fermi_energy = 0
-Nx = 7 # systemsize
-Ny = 7 #systemsize
+Nx = 30 # systemsize
+Ny = 30 #systemsize
 
-Rx = 3 #crosshair pos
-Ry = 3 #crosshair pos
-r = 3 #Chern number=1 domain radius
+Rx = 15 #crosshair pos
+Ry = 15 #crosshair pos
+r = 10 #Chern number=1 domain radius
 
 filepath = "/Users/sogenikegami/Documents/UT4S/non-crystal/crosshair/image/"
 
@@ -114,7 +115,33 @@ def eigenergy(H,imgname):
     ax = fig.add_subplot(1,1,1)
     ax.scatter(num,eigenergy,s=10)
     fig.show()
+    plt.savefig(filepath +  imgname)
+
+def plotC(vertexdata,local_c,filepath,imgname,Nx,Ny,r):
+    N = len(vertexdata)
+    X = []
+    Y = []
+    theta = np.linspace(0,2*math.pi,200)
+    circlex = Nx/2 + r * np.cos(theta)
+    circley = Ny/2 + r * np.sin(theta)
+    fig = plt.figure(figsize=(8.66+2,10))
+    ax = fig.add_subplot(1,1,1)
+    for i in range(N):
+        X.append(vertexdata[i]["pos"][0])
+        Y.append(vertexdata[i]["pos"][1])
+        for j in range(len(vertexdata[i]["neighbor"])):
+            neinum = vertexdata[i]["neighbor"][j]
+            neipos = vertexdata[neinum]["pos"]
+            nowpos = vertexdata[i]["pos"]
+            plt.plot([nowpos[0],neipos[0]],[nowpos[1],neipos[1]],color="black",linewidth = 1)
+    markermax = max(np.abs(local_c))
+    mappable = ax.scatter(X,Y,c=local_c,cmap='bwr',s=800*np.abs(local_c)/markermax,alpha=1,linewidth=0.5,edgecolors='black')
+    fig.colorbar(mappable,ax=ax)
+    ax.plot(circlex,circley,color = "green",linestyle = "dashed")
+    fig.show()
     plt.savefig(filepath + imgname)
+    return 0
+
 
 def main():
     vertexdata = cartesian(Nx,Ny,1)
@@ -122,11 +149,13 @@ def main():
     imgname = "QWZ-model_localC_from_ch01"
     imgname2 = "QWZ_model_crosshair_marker04" 
     #crosshair_marker01.plot(H,vertexdata,fermi_energy,Rx,Ry,filepath,imgname2)
-    crosshair_marker01.local_marker(H,vertexdata,fermi_energy,filepath,imgname)
+    #crosshair_marker01.local_marker(H,vertexdata,fermi_energy,filepath,imgname)
     #eigenergy(H,imgname2)
 
-    imgname3 = "QWZ-localC01"
-    local_chern_marker01.plot(H,vertexdata,fermi_energy,filepath,imgname3)
+    local_c = local_chern_marker01.local_chern_marker(H,vertexdata,fermi_energy)
+    imgname3 = "QWZ-localC02"
+    plotC(vertexdata,local_c,filepath,imgname3,Nx,Ny,r)
+    #local_chern_marker01.plot(H,vertexdata,fermi_energy,filepath,imgname3)
     return 0
 
 
